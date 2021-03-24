4377c8ba583870fc43a7b42c248d80c1a70dd155
wzheng10 <wzheng100@gmail.com>
2021-03-24 13:36:30 -0700

    Testing = Good

--- aa_question/import_db.sql
+++ aa_question/import_db.sql
@@ -1,36 +1,36 @@
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS users;
 
 PRAGMA foreign_keys = ON;
 
-CREATE TABLE users(
+CREATE TABLE users (
     id INTEGER PRIMARY KEY,
     fname TEXT NOT NULL, 
-    lname TEXT NOT NULL,
+    lname TEXT NOT NULL
 );
 
-CREATE TABLE questions(
+CREATE TABLE questions (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    author_id INTEGER PRIMARY
+    author_id INTEGER NOT NULL,
 
-    FOREIGN KEY(author_id) 
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
 INSERT INTO 
-users(fname,lname)
+    users(fname, lname)
 
 VALUES 
 ("Wei","Zheng"),("Ala","Benotman"),("App","Academy");
 
 INSERT INTO 
-questions(title,body,author_id)
+    questions(title, body, author_id)
 
 VALUES 
-("Wei Question","WEI WEI WEI",1),
-("Ala Question","ALA ALA ALA",2),
-("App Question","APP APP APP",3);
+("Wei Question","WEI WEI WEI",(SELECT id FROM users WHERE fname = "Wei")),
+("Ala Question","ALA ALA ALA",(SELECT id FROM users WHERE fname = "Ala")),
+("App Question","APP APP APP",(SELECT id FROM users WHERE fname = "App"));
 
 
 
